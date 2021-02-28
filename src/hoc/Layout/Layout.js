6196dff15fadb598a43157e9c21a9c90598fc637
Armend Krasniqi <armendkras1@gmail.com>
2021-02-28 21:44:28 +0100

    burgerApp

---
+++ src/hoc/Layout/Layout.js
@@ -0,0 +1,45 @@
+import React, { useState } from 'react';
+import { connect } from 'react-redux';
+
+import Aux from '../Auxx/Auxx';
+import classes from './Layout.module.css';
+import Toolbar from '../../components/Navigation/Toolbar/Toolbar';
+import SideDrawer from '../../components/Navigation/SideDrawer/SideDrawer';
+
+const Layout = props =>{
+
+const [sideDrawerIsVisible, setSideDrawerIsVisible] = useState(false);
+  
+const sideDrawerCloseHandler = () => {
+        setSideDrawerIsVisible(false);
+    }
+const  sideDrawerToggleHandler =() => {
+        setSideDrawerIsVisible(!sideDrawerIsVisible);
+        }
+    
+
+
+        return (
+        <Aux>
+        <Toolbar 
+        isAuth={props.isAuthenticated}
+        drawerToggleClicked={sideDrawerToggleHandler} />
+            
+        <SideDrawer 
+        isAuth={props.isAuthenticated}
+        open={sideDrawerIsVisible} 
+        closed={sideDrawerCloseHandler}/>
+        <main className={classes.Content}>
+            {props.children}
+        </main>
+    </Aux>
+        )
+}
+
+
+const mapStateToProps = state => {
+    return {
+        isAuthenticated: state.auth.token !== null
+    }
+}
+export default connect (mapStateToProps)(Layout);
